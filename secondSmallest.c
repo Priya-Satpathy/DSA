504ee7093fba89a8272fbcb42444baf47a5396fe
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-03-11 23:23:54 +0530

    content added

---
+++ secondSmallest.c
@@ -0,0 +1,45 @@
+//find the 2nd smallest
+
+
+#include<stdio.h>
+int main()
+{
+    int a[100], i, size, min=0,minSecond=0;
+    
+
+    printf("Write the total numbers you want to add: ");
+    scanf("%d",&size);
+    
+    printf("Enter the %d numbers:",size);
+    for(i = 0; i < size; i++)
+    {
+        scanf("%d",&a[i]);
+       
+    }
+    
+	min = a[0];
+	for(i = 1; i < size; i++)
+    {
+       if(a[i]<min)
+	   {
+    		min = a[i];
+	   }
+      
+    }
+  
+   	  // Find second minimum
+    for(i = 0; i < size; i++)
+    {
+        if(a[i] > min  &&  a[i] != minSecond)
+        {
+            minSecond = a[i];
+        }
+    }
+
+   	  printf("Minimum is = %d\n",min);
+    printf("Second Minimum is = %d",minSecond);
+
+
+
+   
+}
